1b2ee973be06dda3c84932718e839967fba2f2d1
Wilma Wiker <wikerwilma@gmail.com>
2023-12-11 10:08:05 +0100

    reset

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
-const exerciseRoutes = require("./src/routes/exerciseRoutes");
 const app = express();
 const PORT = 4000;
 
@@ -9,25 +6,9 @@ app.get("/home", (req, res) => {
   res.status(200).json("Welcome, your app is working well");
 });
 
-app.get("/api/v1/exercises", exerciseRoutes);
-
-async function run() {
-  try {
-    // Connect to MongoDB database (via Mongoose)
-    mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-
-    // Start server; listen to requests on port
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-run();
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
 
 // Export the Express API
 module.exports = app;
--- src/routes/exerciseRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllExercises } = require("../controllers/exerciseController");
-
-router.get("/", getAllExercises);
-
-module.exports = router;
