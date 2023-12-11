39f9016a7540732f7cc51bba48e849f3d459f1b9
Wilma Wiker <wikerwilma@gmail.com>
2023-12-11 11:54:50 +0100

    test

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -3,7 +3,6 @@ const Exercise = require("../models/Exercise");
 exports.getAllExercises = async (req, res) => {
   try {
     const exercises = await Exercise.find();
-    if (!Exercise) throw new NotFoundError("No exercises found");
     return res.json({
       data: exercises,
     });
